66ccecf52187c5a3c8dc61ffada3cea7a20b2eb0
Papoppop <47813.papop@gmail.com>
2025-10-30 16:28:16 +0700

    Added Pavo: 4th Character

--- lib/game/components/player_character.dart
+++ lib/game/components/player_character.dart
@@ -18,9 +18,9 @@ extension PlayerCharacterExtension on PlayerCharacter {
       case PlayerCharacter.character2:
         return 'characters/EiraPlayable.png';
       case PlayerCharacter.character3:
-        return 'characters/AnsiaPlayable.png'; // Add path when sprite sheet is ready
+        return 'characters/AnsiaPlayable.png'; 
       case PlayerCharacter.character4:
-        return null; // Add path when sprite sheet is ready
+        return 'characters/PavoPlayable.png'; 
     }
   }
 
@@ -34,7 +34,7 @@ extension PlayerCharacterExtension on PlayerCharacter {
       case PlayerCharacter.character3:
         return 'assets/images/characters/AnsiaPlayable.png';
       case PlayerCharacter.character4:
-        return 'assets/images/characters/character4.png';
+        return 'assets/images/characters/PavoPlayable.png';
     }
   }
 
